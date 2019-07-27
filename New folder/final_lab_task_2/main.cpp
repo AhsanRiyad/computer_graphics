c16f7f90ea0e14ab98fb2ad172ca78bb5514561f
AhsanRiyad <riyad298@gmail.com>
2019-07-27 17:07:32 +0600

    commit by alias
    this is used for short cut puspose

--- New folder/final_lab_task_2/main.cpp
+++ New folder/final_lab_task_2/main.cpp
@@ -5,11 +5,24 @@
 #include<bits/stdc++.h>
 #define PI acos(-1.0)
 
+#include<iostream>
+
+using namespace std;
 
 GLfloat position = 0.0f;
 GLfloat speed = 0.1f;
 float angle=45.0;
 
+GLfloat init_point = 0.0f;
+
+GLfloat first_point_x =  -0.06f ;
+GLfloat first_point_y =  -0.6f ;
+
+GLfloat position_stone_x = 0.0f;
+GLfloat position_stone_y = 0.0f;
+GLfloat speed_stone = 0.0f;
+
+bool up_click_check = false ;
 
 void update(int value) {
 
@@ -17,34 +30,45 @@ void update(int value) {
         position = -1.2f;
     }
 
+    if(up_click_check == true){
+      position_stone_y += .1f ;
+    }
+
+
 
     //position += speed;
 
-	glutPostRedisplay();
+  glutPostRedisplay();
 
 
-	glutTimerFunc(100, update, 0);
+  glutTimerFunc(100, update, 0);
 }
 void init() {
    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 }
 
-void handleMouse(int button, int state, int x, int y) {
-	if (button == GLUT_LEFT_BUTTON)
-	{
-		if (state == GLUT_DOWN)
-		{
-			speed += 0.1f;
-			printf("clicked at (%d, %d)\n", x, y);
-		}
-	}
 
-	glutPostRedisplay();
+
+
+void handleMouse(int button, int state, int x, int y) {
+  if (button == GLUT_LEFT_BUTTON)
+  {
+    if (state == GLUT_DOWN)
+    {
+      speed += 0.1f;
+      printf("clicked at (%d, %d)\n", x, y);
+    }
+  }
+
+  glutPostRedisplay();
 }
+
+
+
 
 void handleKeypress(unsigned char key, int x, int y) {
 
-	switch (key) {
+  switch (key) {
 
 case 'a':
     //speed = 0.0f;
@@ -59,26 +83,43 @@ case 'w':
 glutPostRedisplay();
 
 
-	}
+  }
 }
 
-void
-catchKey(int key, int x, int y)
+void catchKey(int key, int x, int y)
 {
     if(key == GLUT_KEY_LEFT){
         position -= .1f;
+
+        first_point_x -= .1f ;
+
+
+    init_point = (first_point_x + first_point_x)/2 ;
+
+    cout<< init_point ;
+
         printf("Left key is pressed\n");
     }
 
     else if(key == GLUT_KEY_RIGHT){
+
         position += .1f;
+        first_point_x += .1f ;
+
+        init_point = first_point_x + (first_point_x) ;
+
         printf("Right key is pressed\n");
     }
 
     else if(key == GLUT_KEY_DOWN)
         printf("Down key is pressed\n");
-    else if(key == GLUT_KEY_UP)
+    else if(key == GLUT_KEY_UP){
+
+        up_click_check = true ;
+        //position_stone= 0.2f ;
         printf("Up key is pressed\n");
+    }
+
 }
 
 
@@ -86,11 +127,11 @@ catchKey(int key, int x, int y)
 
 void drawCircle(float x,float y,float radius)
 {
-	int i;
-	int lineAmount = 100;
-	GLfloat twicePi = 2.0f * PI;
-	glBegin(GL_TRIANGLE_FAN);
-	glVertex2f(x,y);
+  int i;
+  int lineAmount = 100;
+  GLfloat twicePi = 2.0f * PI;
+  glBegin(GL_TRIANGLE_FAN);
+  glVertex2f(x,y);
 
     for(i = 0; i <= lineAmount;i++)
     {
@@ -98,22 +139,36 @@ void drawCircle(float x,float y,float radius)
         float tmpy=y + (radius* sin(i * twicePi / lineAmount));
         glVertex2f(tmpx,tmpy);
     }
-	glEnd();
+  glEnd();
 }
 
 
 
 void display() {
+
+
+
+
+
    glClear(GL_COLOR_BUFFER_BIT);
    glLoadIdentity();
 
 
-glPushMatrix();
-glTranslatef(position,0.0f, 0.0f);
+
+    glPushMatrix();
+    position_stone_x = position ;
+    glTranslatef(position_stone_x ,position_stone_y, 0.0f);
+     //circle
+    drawCircle(0.0f,-.225f , .08f);
+
+    glPopMatrix();
 
 
-    //circle
- drawCircle(-.00f,-.225f , .08f );
+
+    glPushMatrix();
+    glTranslatef(position,0.0f, 0.0f);
+
+    //drawCircle(-.00f,-.225f , .08f );
 
 
    //first quads
--- final_lab_task_1.cpp
+++ final_lab_task_1.cpp
@@ -2,92 +2,263 @@
 
 #include <GL/gl.h>
 #include <GL/glut.h>
+#include<bits/stdc++.h>
+#define PI acos(-1.0)
 
+#include<iostream>
+
+using namespace std;
 
 GLfloat position = 0.0f;
 GLfloat speed = 0.1f;
+float angle=45.0;
+
+GLfloat init_point = 0.0f;
+
+GLfloat first_point_x =  -0.06f ;
+GLfloat first_point_y =  -0.6f ;
+
+GLfloat position_stone_x = 0.0f;
+GLfloat position_stone_y = 0.0f;
+GLfloat speed_stone = 0.0f;
+
+bool up_click_check = false ;
 
 void update(int value) {
 
-    if(position > 1.0)
+    if(position > 1.0){
         position = -1.2f;
+    }
 
-    position += speed;
+    if(up_click_check == true){
+      position_stone_y += .1f ;
+    }
 
-	glutPostRedisplay();
 
 
-	glutTimerFunc(100, update, 0);
+    //position += speed;
+
+  glutPostRedisplay();
+
+
+  glutTimerFunc(100, update, 0);
 }
 void init() {
    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 }
 
-void handleMouse(int button, int state, int x, int y) {
-	if (button == GLUT_LEFT_BUTTON)
-	{
-		if (state == GLUT_DOWN)
-		{
-			speed += 0.1f;
-			printf("clicked at (%d, %d)\n", x, y);
-		}
-	}
 
-	glutPostRedisplay();
+
+
+void handleMouse(int button, int state, int x, int y) {
+  if (button == GLUT_LEFT_BUTTON)
+  {
+    if (state == GLUT_DOWN)
+    {
+      speed += 0.1f;
+      printf("clicked at (%d, %d)\n", x, y);
+    }
+  }
+
+  glutPostRedisplay();
 }
+
+
+
 
 void handleKeypress(unsigned char key, int x, int y) {
 
-	switch (key) {
+  switch (key) {
 
 case 'a':
-    speed = 0.0f;
+    //speed = 0.0f;
+    position += .1f;
     break;
 case 'w':
-    speed += 0.1f;
+    //speed += 0.1f;
+    position -= .1f;
     break;
 
 
 glutPostRedisplay();
 
 
-	}
+  }
+}
+
+void catchKey(int key, int x, int y)
+{
+    if(key == GLUT_KEY_LEFT){
+        position -= .1f;
+
+        first_point_x -= .1f ;
+
+
+    init_point = (first_point_x + first_point_x)/2 ;
+
+    cout<< init_point ;
+
+        printf("Left key is pressed\n");
+    }
+
+    else if(key == GLUT_KEY_RIGHT){
+
+        position += .1f;
+        first_point_x += .1f ;
+
+        init_point = first_point_x + (first_point_x) ;
+
+        printf("Right key is pressed\n");
+    }
+
+    else if(key == GLUT_KEY_DOWN)
+        printf("Down key is pressed\n");
+    else if(key == GLUT_KEY_UP){
+
+        up_click_check = true ;
+        //position_stone= 0.2f ;
+        printf("Up key is pressed\n");
+    }
+
 }
 
 
+
+
+void drawCircle(float x,float y,float radius)
+{
+  int i;
+  int lineAmount = 100;
+  GLfloat twicePi = 2.0f * PI;
+  glBegin(GL_TRIANGLE_FAN);
+  glVertex2f(x,y);
+
+    for(i = 0; i <= lineAmount;i++)
+    {
+        float tmpx=x + (radius * cos(i *  twicePi / lineAmount));
+        float tmpy=y + (radius* sin(i * twicePi / lineAmount));
+        glVertex2f(tmpx,tmpy);
+    }
+  glEnd();
+}
+
+
+
 void display() {
+
+
+
+
+
    glClear(GL_COLOR_BUFFER_BIT);
    glLoadIdentity();
 
 
 
+    glPushMatrix();
+    position_stone_x = position ;
+    glTranslatef(position_stone_x ,position_stone_y, 0.0f);
+     //circle
+    drawCircle(0.0f,-.225f , .08f);
 
-   glBegin(GL_QUADS);
-      glColor3f(1.0f, 0.0f, 0.0f);
-      glVertex2f(-0.2f, -0.2f);
-      glVertex2f( 0.2f, -0.2f);
-      glVertex2f( 0.2f,  0.2f);
-      glVertex2f(-0.2f,  0.2f);
+    glPopMatrix();
+
+
+
+    glPushMatrix();
+    glTranslatef(position,0.0f, 0.0f);
+
+    //drawCircle(-.00f,-.225f , .08f );
+
+
+   //first quads
+
+ glBegin(GL_QUADS);
+      glColor3ub(204,127,50);
+      glVertex2f(-0.06f, -0.6f);
+      glVertex2f( 0.06f, -0.6f);
+      glVertex2f( 0.06f,  -0.3f);
+      glVertex2f(-0.06f, -0.3f);
    glEnd();
 
 
 
 
-glPushMatrix();
-glTranslatef(position,0.0f, 0.0f);
-   glBegin(GL_QUADS);
-      // glColor3f(1.0f, 0.0f, 0.0f);
-      // glVertex2f(-0.2f, -0.2f);
-      // glVertex2f( 0.2f, -0.2f);
-      // glVertex2f( 0.2f,  0.2f);
-      // glVertex2f(-0.2f,  0.2f);
+   //first lej
+
+    glBegin(GL_TRIANGLES);
+      glColor3ub(255,63,0);
+      glVertex2f( -0.125f, -0.4f);
+      glVertex2f( -0.06f,  -0.4f);
+      glVertex2f(-0.06f, -0.3f);
    glEnd();
+
+
+
+
+
+    glBegin(GL_TRIANGLES);
+      glColor3ub(255,63,0);
+      glVertex2f( 0.125f, -0.4f);
+      glVertex2f( 0.06f,  -0.4f);
+      glVertex2f(0.06f, -0.3f);
+   glEnd();
+
+
+
+    //second lej
+
    glBegin(GL_TRIANGLES);
-   // glColor3f(0.0f, 0.0f, 1.0f);
-   // glVertex2f(0.2f,-0.2f);
-   // glVertex2f(0.5f, 0.0f);
-   // glVertex2f(0.2f, 0.2f);
+      glColor3ub(255,63,0);
+      glVertex2f( -0.1f, -0.6f);
+      glVertex2f( -0.06f,  -0.6f);
+      glVertex2f(-0.06f, -0.5f);
    glEnd();
+
+
+   glBegin(GL_TRIANGLES);
+      glColor3ub(255,63,0);
+      glVertex2f( 0.1f, -0.6f);
+      glVertex2f( 0.06f,  -0.6f);
+      glVertex2f(0.06f, -0.5f);
+   glEnd();
+
+
+
+    //first triangle under quads
+
+   glBegin(GL_POLYGON);
+      glColor3ub(255,63,0);
+      glVertex2f( 0.1f, -0.7f);
+      glVertex2f( 0.0f,  -.9f);
+      glVertex2f(-0.1f, -0.7f);
+
+      glVertex2f(0.06f, -0.6f);
+      glVertex2f(-0.06f, -0.6f);
+
+
+      glVertex2f(-0.1f, -0.7f);
+   glEnd();
+
+
+
+
+    //second triangle under quads
+
+   glBegin(GL_POLYGON);
+      glColor3ub(255,208,0);
+      glVertex2f( 0.07f, -0.7f);
+      glVertex2f( 0.0f,  -.8f);
+      glVertex2f(-0.07f, -0.7f);
+      glVertex2f(0.06f, -0.6f);
+      glVertex2f(-0.06f, -0.6f);
+      glVertex2f(-0.07f, -0.7f);
+   glEnd();
+
+
+
+
+
 
 glPopMatrix();
 
@@ -100,12 +271,13 @@ glPopMatrix();
 
 int main(int argc, char** argv) {
    glutInit(&argc, argv);
-   glutInitWindowSize(320, 320);
+   glutInitWindowSize(720, 720);
    glutInitWindowPosition(50, 50);
    glutCreateWindow("Basic Animation");
    glutDisplayFunc(display);
    init();
    glutKeyboardFunc(handleKeypress);
+   glutSpecialFunc(catchKey);
    glutMouseFunc(handleMouse);
    glutTimerFunc(1000, update, 0);
    glutMainLoop();
